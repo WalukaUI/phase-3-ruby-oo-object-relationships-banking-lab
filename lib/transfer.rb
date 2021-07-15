d47b472dd3e79e582655c27e9245af504f6fd92b
WalukaUI <cwaluka@yahoo.com>
2021-07-14 18:36:06 -0700

    completd

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
   # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+ 
+  def initialize(sender,receiver,amount)
+    @sender=sender
+    @receiver=receiver
+    @status="pending"
+    @amount=amount
+  
+  end
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+      else
+      self.reject
+      end
+  end
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    else
+      self.reject
+    end
+  end
+  def reject
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end
